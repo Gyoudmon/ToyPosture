97588cf9d6c141e9b8b9034f3663ed7a0d2ed611
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2018-06-28 18:14:00 +0800

    eliminate ambiguity

--- villages/chapter01/e62.rkt
+++ villages/chapter01/e62.rkt
@@ -11,17 +11,18 @@
 (define n-1 (- n 1))
 (define s² (* s s))
 (define Σx (* n mean))
-(define Σx² (* Σx Σx))
+(define Σ²x (* Σx Σx))
 (define x1²+x4² (+ (* x1 x1) (* x4 x4)))
 
 (define x2+x3 (- Σx x1 x4))
-(define x2²+x3² (- (+ (* n-1 s²) (/ Σx² n)) x1²+x4²))
+
+;;; (n-1)S² = Σx² - (Σ²x)/n
+(define x2²+x3² (- (+ (* n-1 s²) (/ Σ²x n)) x1²+x4²))
 
 (define α x2+x3)
 (define β x2²+x3²)
 
 ;;; 2x² - 2αx + (α² - β) = 0
-
 (define a 2)
 (define b (* -2 α))
 (define c (- (* α α) β))
