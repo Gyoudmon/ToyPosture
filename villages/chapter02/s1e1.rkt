35efa46bd99e3c76c0a22d11bc17fecc12291501
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2018-07-03 18:26:02 +0800

    finish s1e1 of chapter02

--- villages/chapter02/s1e1.rkt
+++ villages/chapter02/s1e1.rkt
@@ -1,7 +1,5 @@
 #lang racket
 
-(define sample-space (make-hasheq))
-
 (define seed (make-parameter 1024))
 
 (define match
@@ -9,19 +7,26 @@
     (if (even? (random (seed))) (values p1 p2) (values p2 p1))))
 
 (define tournament
-  (lambda []
+  (lambda [S]
     (define-values (r11:w r11:l)  (match     1     2))
     (define-values (r12:w r12:l)  (match     3     4))
     (define-values (r21:w r21:l)  (match r11:w r12:w))
     (define-values (r22:w r22:l)  (match r11:l r12:l))
-    (define-values (winner loser) (match r21:w r22:w))
     
-    (define outcome (+ (* r21:w 1000) (* r21:l 100) (* r22:w 10) (* r22:l 1)))
+    (set-add S (+ (* r21:w 1000) (* r21:l 100)
+                  (* r22:w 10) (* r22:l 1)))))
 
-    (hash-set! sample-space outcome (add1 (hash-ref sample-space outcome 0)))))
 
+(define S
+  (for/fold ([S (seteq)])
+            ([i (in-range 1000)])
+    (tournament S)))
 
-(for ([i (in-range 1000)]) (tournament))
-
-(define S (hash-keys sample-space))
-(list (length S) S) ;;; 2 * (2*2 + 2*2)
+(set-count S) ;;; 2 * (2*2 + 2*2)
+(define A (list->seteq (filter (λ [s] (< s 2000)) (set->list S))))
+(define B (list->seteq (filter (λ [s] (or (< 2000 s 3000) (< 200 (remainder s 1000) 300))) (set->list S))))
+A
+B
+(set-union A B)
+(set-intersect A B)
+(set-subtract S A)
