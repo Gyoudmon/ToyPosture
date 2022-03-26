a6e6680295c707989b322c905cf9315388d95f1f
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2022-03-27 00:02:05 +0800

    add struct subproject

---
+++ struct/info.rkt
@@ -0,0 +1,10 @@
+#lang info
+
+(define collection 'use-pkg-name)
+(define pkg-desc "Data Structure")
+
+(define version "1.0")
+(define pkg-authors '(wargrey))
+(define test-omit-paths 'all)
+
+(define native-launcher-names '(["linked_list.c" console]))
