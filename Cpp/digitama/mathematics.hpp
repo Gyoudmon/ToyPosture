33ae74a5b0b3338e13f9210be074bf59d37ec7fb
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2022-10-06 16:23:19 +0800

    work with submodule

--- Cpp/digitama/mathematics.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef WARGREY_STEM_MATH
-#define WARGREY_STEM_MATH
-
-namespace WarGrey::STEM {
-    float radians_to_degrees(float degrees);
-    float degrees_to_radians(float degrees);
-}
-
-#endif
